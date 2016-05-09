f039d32aab0f3a759ee7dcceefa0cb2585ecdf09
sarahbat <sarahbat@gmail.com>
2016-05-08 20:56:36 -0700

    Clean up to bin definition; add support for multiple labels

--- js/binMaster.js
+++ js/binMaster.js
@@ -7,7 +7,18 @@ var binMaster = {};
 binMaster.Bin = function () {
 	this.numericDefn = new binMaster.Bin_NumericDefn();
 	this.encodeDefn = new binMaster.Bin_EncodeDefn();
-	this.labelDefn = new binMaster.Bin_LabelDefn("DEFAULT");
+	this.labels = {}; // muliple labels can be used for any Bin
+	// var _this = this;
+
+	// get/set labels and initialize default label
+	this.set_LabelDef = (function(definitionName) {
+		this.labels[definitionName] = {minLabel: "undefined",maxLabel: "undefined",rangeLabel: "undefined"} ; // a bin may have multiple labels
+	});
+	this.get_LabelDef = (function(definitionName) {
+		return this.labels[definitionName];
+	});
+
+	this.set_LabelDef('DEFAULT'); // initialize default label properties
 };
 
 /**
@@ -24,20 +35,3 @@ binMaster.Bin_NumericDefn = function(){
 binMaster.Bin_EncodeDefn = function(){
 	this.color = undefined;
 };
-
-/**
- * semantic definition for one bin.  Each bin may have multiple definitions.
- */
-binMaster.Bin_LabelDefn = function(definitionName){
-	this.labels = {}; 
-	this.labels[definitionName] = binMaster.set_LabelDefn();
-}
-
-binMaster.set_LabelDefn = function(){
-	return {minLabel: "undefined",maxLabel: "undefined",rangeLabel: "undefined"} ; // a bin may have multiple labels
-}
-
-binMaster.add_LabelDefn = function(definitionName){
-	// add a new label defnition in the labels object
-}
-
