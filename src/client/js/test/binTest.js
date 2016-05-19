5a7e6932a68e91b9a33fb03fd62404f3e2493962
sarahbat <sarahbat@gmail.com>
2016-05-19 16:34:09 -0700

    updates to (manual) test file

--- src/client/js/test/binTest.js
+++ src/client/js/test/binTest.js
@@ -1,27 +1,42 @@
 // assorted functionality tests
 
 var binTest = {};
+var bTest = {};
+
+function makeTestData(numberBins){
+	if (numberBins === undefined){numberBins = 5;}
+	bTest = new binMaster.BinList(numberBins);
+
+	// add some values to the bins 
+	var STEP_SIZE = 5;
+	for (var i = 0; i < numberBins; i++){
+		var minVal = STEP_SIZE * i;
+		bTest.bins[i].setBinMin(minVal);
+		bTest.bins[i].setBinMax(minVal + STEP_SIZE);
+	}
+}
+
 
 function testBins(){
 	// make a 5 class binList 
-	var binTest = new binMaster.BinList(5);
+	var binListTest = new binMaster.BinList(5);
 
 	// add some values to the bins 
 	var STEP_SIZE = 5;
 	for (var i = 0; i < 5; i++){
 		var minVal = STEP_SIZE * i;
-		binTest.bins[i].setBinMin(minVal);
-		binTest.bins[i].setBinMax(minVal + STEP_SIZE);
+		binListTest.bins[i].setBinMin(minVal);
+		binListTest.bins[i].setBinMax(minVal + STEP_SIZE);
 	}
 
 	// find which bin has which values
-	binTest.getBin(12);
+	binListTest.getBin(12);
 
 	// test out with the dataMgr and an array of breaks
 	var b = new binMaster.BinList([0, 1000, 5000, 10000, 25000, 50000, 100000])
 	var data = binMaster.dataMgr.getColumn('FIPS');
 	for (var i = 0; i < data.length; i++) {
-		console.log(data[i], "is in class", b.getBin(data[i]));
+		console.log(data[i], 'is in class', b.getBin(data[i]));
 	}
 
 
@@ -38,6 +53,8 @@ function loadMapTestData(){
 
 function testMap(breakType){
 	if (breakType === undefined){ breakType = 'EqualInterval';}
+	
+	binMaster.dataMgr.key = 'FIPS';
 
 	d3.select('.test').remove();
 	map_div = d3map.makeMapDiv(null, 'test', 500, 500); // 500px square div
@@ -47,12 +64,14 @@ function testMap(breakType){
 	binTest.breaks = binMaster.dataMgr.getBins('PCT_04', breakType, 5);
 	binTest.bins = new binMaster.BinList(binTest.breaks);
 	binTest.binnedData = [];
-	binTest.id = binMaster.dataMgr.getColumn('FIPS');
+	binTest.id = binMaster.dataMgr.getColumn(binMaster.dataMgr.key); // retrieve column set as key field
 	binTest.data = binMaster.dataMgr.getColumn('PCT_04');
+
+	binTest.bins.setColorEncoding(5);
 
 	for (var i = 0; i < binTest.data.length; i++) {
 		binTest.binnedData.push({'id': [binTest.id[i]], 'value': binTest.bins.getBin(binTest.data[i])});
 	}
 
-	d3map.encodeMap('counties', binTest.binnedData, 'YlGn', binTest.breaks.length-1); // encode and link FIPS code to NUM_04 attribute
+	d3map.encodeMap('counties', binTest.binnedData, binTest.bins); // encode and link FIPS code to NUM_04 attribute
 }
